ea8cbf267d0e35ba3ee09583d1a03aa3fe46aa83
Thilo Umland <thumland@uni-bremen.de>
2026-07-31 01:44:33 +0200

    Some additions to the evaluation notebook

--- compare-python/analyze/analyze_results_archive_aggregated.py
+++ compare-python/analyze/analyze_results_archive_aggregated.py
@@ -69,8 +69,7 @@ for pc_name in os.listdir(result_path):
 
     python_short_names = {}
     python_data = []
-    for key in ["Pybullet", "distance3d Nesterov (Primitives with acceleration)", "distance3d Nesterov (Primitives)",
-                "distance3d Nesterov (with acceleration)", "distance3d Nesterov",
+    for key in ["Pybullet", "distance3d Nesterov (with acceleration)", "distance3d Nesterov",
                 "distance3d Jolt (distance)", "distance3d Jolt (intersection)", "distance3d Original"]:
         python_short_names[key] = short_names[key]
         python_data.append(results[key])
